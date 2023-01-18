6efaaaf761c6533225328bed7fc0c4af97504422
Gustavo Queiroz <guaraujoo@hotmail.com>
2023-01-18 17:01:30 -0300

    add findAll and findById methods

--- src/main/java/com/estudosjava/springbootcourse/repositories/UserRepository.java
+++ src/main/java/com/estudosjava/springbootcourse/repositories/UserRepository.java
@@ -1,10 +1,10 @@
 package com.estudosjava.springbootcourse.repositories;
 
 import com.estudosjava.springbootcourse.entities.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository // opcional porque o JPARepository ja faz isso
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/com/estudosjava/springbootcourse/resources/UserResource.java
+++ src/main/java/com/estudosjava/springbootcourse/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.estudosjava.springbootcourse.resources;
 
 import com.estudosjava.springbootcourse.entities.User;
+import com.estudosjava.springbootcourse.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(null, "Gustavo", "guaraujoo@hotmail.com", "11961022720", "teste");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
 
 }
