79315f9e122eaed8d3985256818ddf355b794f4a
Gustavo Queiroz <guaraujoo@hotmail.com>
2023-02-03 18:12:42 -0300

    Order item PARTE 1

---
+++ src/main/java/com/estudosjava/springbootcourse/entities/pk/OrderItemPK.java
@@ -0,0 +1,55 @@
+package com.estudosjava.springbootcourse.entities.pk;
+
+import com.estudosjava.springbootcourse.entities.Order;
+import com.estudosjava.springbootcourse.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable // para classe auxiliar de chave composta
+public class OrderItemPK implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK orderItem = (OrderItemPK) o;
+        return Objects.equals(order, orderItem.order) && Objects.equals(product, orderItem.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
+
+
